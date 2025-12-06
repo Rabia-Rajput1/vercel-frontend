22b5880c6d7533fa11006c13cbaed36b30ffaf9f
Rabia-Rajput1 <rabiashahid3311@gmail.com>
2025-12-06 12:36:19 +0500

    prevent build from hanging

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  build: {
+    minify: "esbuild",    // FIX: prevents build from hanging
+    target: "esnext",     // faster build + supports modern JS
+    outDir: "dist",       // Vercel expects this folder
+    sourcemap: false,     // optional optimization
+  }
 });
